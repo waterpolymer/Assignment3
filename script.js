24b47508ae57a40808a417102eaed98dc55813b9
waterpolymer <kendrick.shao28@myhunter.cuny.edu>
2020-01-06 13:41:00 -0800

    correctly formatted answers

--- script.js
+++ script.js
@@ -2,19 +2,24 @@
 let container = document.getElementById("container");
 
 // 2) Select the section with an id of container using querySelector.
-let container = document.querySelector(".container");
+let container2 = document.querySelector("#container");
+console.log(container2);
 
 // 3) Select all of the list items with a class of "second".
-let second = document.querySelectorAll(".second");
+let second = document.querySelector("li.second");
+console.log(second);
 
 // 4) Select a list item with a class of third, but only the list item inside of the ol tag.
-container.lastElementChild.querySelector(".third");
+let third = container.lastElementChild.querySelector("li.third");
+console.log(third);
 
 // 5) Give the section with an id of container the text "Hello!".
-container.innerText = "Hello!";
+let helloText = document.createTextNode("Hello");
+container.appendChild(helloText);
 
 // 6) Add the class main to the div with a class of footer.
-let footer = document.getElementById("footer")
+let footer = document.querySelector("div.footer");
+console.log(footer);
 footer.classList.add("main");
 
 // 7) Remove the class main on the div with a class of footer.
@@ -24,11 +29,19 @@ footer.classList.remove("main");
 let newLi = document.createElement("li");
 
 // 9) Give the li the text "four".
-let text = document.createElement("a");
-text = document.createTextNode("four");
+let text = document.createTextNode("four");
+newLi.appendChild(text);
 
 // 10) Append the li to the ul element.
+let ol = container.firstElementChild
+ol.appendChild(newLi);
 
 // 12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+for(let i = 0; i < ol.children.length; i++){
+    //console.log(child);
+    ol.children[i].style.backgroundColor = "green";
+}
+
 
 // 13) Remove the div with a class of footer.
+footer.remove;
